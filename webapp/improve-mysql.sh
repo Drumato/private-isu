a315acf9a8f435565b9f09548199c9492645f17e
drumato <drumato43@gmail.com>
2023-06-21 11:29:43 +0900

    add user_index to comment table
    Signed-off-by: drumato <drumato43@gmail.com>

--- webapp/improve-mysql.sh
+++ webapp/improve-mysql.sh
@@ -1,3 +1,4 @@
 set -ux
 docker exec webapp-mysql-1 mysql -proot -e "use isuconp; ALTER TABLE comments ADD INDEX post_id_index(post_id);"
+docker exec webapp-mysql-1 mysql -proot -e "use isuconp; ALTER TABLE comments ADD INDEX user_id_index(user_id);"
 docker exec webapp-mysql-1 mysql -proot -e "use isuconp; ALTER TABLE posts ADD INDEX created_at_desc_index(created_at DESC);"
